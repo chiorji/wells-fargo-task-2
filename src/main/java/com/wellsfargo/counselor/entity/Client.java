189d308b31023d865cb9ed0e780c812514f76904
Chigbogu Orji <brightorji60@gmail.com>
2026-06-30 02:26:33 +0100

    implement client class, with getters and setters

---
+++ src/main/java/com/wellsfargo/counselor/entity/Client.java
@@ -0,0 +1,94 @@
+package com.wellsfargo.counselor.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
+public class Client {
+    @Id
+    @GeneratedValue
+    private long clientId;
+
+    @Column(nullable = false)
+    private long advisorId;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String phone;
+
+    @Column(nullable = false)
+    private String address;
+
+    protected Client() {}
+
+    public Client(String firstName, String lastName, String email, String phone, String address, Long advisorId) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.phone = phone;
+        this.address = address;
+        this.advisorId = advisorId;
+    }
+
+    public Long getClientId() {
+        return clientId;
+    }
+
+    public Long getAdvisorId() {
+        return advisorId;
+    }
+
+    public void setAdvisorId(long advisorId) {
+        this.advisorId = advisorId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
